fc6a0a008158db7c182758d55b2f161d982674f8
serban-hartular <hartular@gmail.com>
2021-11-08 10:15:44 +0200

    Made parse tree pretty.

--- client/src/SvgTree.svelte
+++ client/src/SvgTree.svelte
@@ -1,29 +1,68 @@
 <script lang="ts">
-import { onMount } from 'svelte';
+import { afterUpdate, onMount } from 'svelte';
 import type { TreeLibrary } from "./parse_tree";
-import  SvgNode from "./svg_utils"
+import  SvgNode, { SvgMap, Line } from "./svg_utils"
 
     export let tree_library : TreeLibrary
 	export let root_id : string
 
-	let node_map : Map<string, SvgNode> = null
+	let node_map : SvgMap = new SvgMap(tree_library, root_id)//SvgNode.generateTree(tree_library, root_id)
+	let node_array : Array<SvgNode>
+	let line_array : Array<Line>
 
 	$: {
-		root_id;
-		node_map = SvgNode.generateTree(tree_library, root_id)
+		if(root_id != node_map.root_id) {
+			node_map  = new SvgMap(tree_library, root_id)
+			console.log('new node_map !')
+		}
+	}
+	$:{
+		// node_map;
+		node_array = Array.from(node_map.values())
+		line_array = node_map.lines
+		console.log('node_map changed!')
+	}
+
+	function updateCoordinates() {
+		for(let node of node_map.values()) {
+			let svg_text : any = document.getElementById(node.id)
+			let bbox = svg_text.getBBox()
+			node.w = bbox.width
+			node.h = bbox.height
+		}
+		node_map.updateCoordinates()
 	}
 
 	onMount(() => {
+		// for(let line of line_array) {
+		// 	line.x1 += 10
+		// }
+		console.log('doing onMount')
+		updateCoordinates()
+		node_map = node_map
 	})
+
+	afterUpdate(() => {
+		console.log('doing afterUpdate')
+		updateCoordinates()
+		node_map = node_map
+	})
+
 	function onClick() {
 	}
 // 	console.log(text1.getComputedTextLength())
+
+	let width : number = 300
+
 </script>
 
 {#if node_map}
 <svg height = "300px">
-	{#each Array.from(node_map.values()) as node}
-	    <text class="node" x={node.x} y={node.y}>{node.text}</text>		
+	{#each node_array as node}
+	    <text class="node" id={node.id} x={node.x} y={node.y}>{node.text}</text>		
+	{/each}
+	{#each line_array as line, i(i) }
+		<line x1={line.x1} y1={line.y1} x2={line.x2} y2={line.y2} stroke="black" />
 	{/each}
 </svg>
 {/if}
--- client/src/svg_utils.ts
+++ client/src/svg_utils.ts
@@ -1,97 +1,148 @@
 
 import  type { Node, TreeLibrary } from "./parse_tree";
 
+type Coordinate = [number, number]
+
 export default class SvgNode {
     static dx : number = 64
     static dy : number = 32
     id : string
     text : string
+    //origin :Coordinate= [0, 0]
     x : number
     y : number
-    private w : number = 0
-    private h : number = 0
+    w : number = 0
+    h : number = 0
+    mid_x : number
+    top_y : number
     row : number
     col : number
-    top : [number, number]
-    bottom : [number, number]
-    svg_element : SVGGraphicsElement
+    //svg_element : SVGGraphicsElement
     children : Array<string>
     constructor(id : string, text : string, children : Array<string>) {
         this.id = id
         this.text = text
         this.children = children
     }
-    setDimensions(width : number, height : number) {
-        this.w = width
-        this.h = height
-        this.calcTopBottom()
-    }
-    getDimensions() : [number, number] {
-        return [this.w, this.h]
-    }
-    getTop() : [number,number] {
-        return this.top
-    }
-    getBottom() : [number, number] {
-        return this.bottom
-    }
     calcTopBottom() {
-        this.top =      [this.x + this.w / 2, this.y + this.h]
-        this.bottom =   [this.x + this.w / 2, this.y]
+        this.mid_x =    this.x + this.w / 2
+        this.top_y =    this.y - this.h / 2
     }
-    static generateTree(tree_library : TreeLibrary, id : string, depth : number = 0) : Map<string, SvgNode> {
-        let node = tree_library.get(id)
-        let svg_node = new SvgNode(node.id, node.type, node.children_ids)
-        let [row, col] = tree_library.position_map.get(id)
-        svg_node.row = depth;
-        svg_node.col = col
-        svg_node.x = svg_node.col * SvgNode.dx
-        svg_node.y = (svg_node.row + 1) * SvgNode.dy
-        let map = new Map<string, SvgNode>()
-        map.set(id, svg_node)
-        // console.log(svg_node.id, depth, svg_node.y)
-        for(let child of svg_node.children) {
-            for(let [k, v] of SvgNode.generateTree(tree_library, child, depth+1))
-                map.set(k, v)
-        }
-        return map
-    }
-    connectToDom(svg_element : SVGGraphicsElement) {
-        this.svg_element = svg_element
-        this.setDimensions(svg_element.getBBox().width, svg_element.getBBox().height)
-    }
-    static generatePositions(map : Map<number, SvgNode>) {
-        let row_width_map = new Map<number, number>()
-        let max_row = 0
-        for (let [id, node] of map) { //find maximum width of each row
-            if(!(row_width_map.get(node.row) > node.w)) {
-                row_width_map.set(node.row, node.w)
-            }
-            max_row = node.row > max_row ? node.row : max_row
-        }
-        let row_limits = new Array<[number, number]>(max_row).fill([0, 0])
-        row_limits[0][1] = row_width_map.get(0)
-        if(row_limits[0][1] == undefined) throw Error('row 0 width undefined')
-        for(let row = 1; row < max_row; row++) {
-            let row_width = row_width_map.get(row)
-            if(row_width == undefined) throw Error('row ' + String(row) + ' width undefined ')
-            row_limits[row][0] = row_limits[row-1][1]
-            row_limits[row][1] = row_limits[row][0] + row_width
-        }
-        for(let [id, node] of map) {
-            let row = node.row
-            node.x = (row_limits[row][0] + row_limits[row][1]) / 2 - node.w / 2
-        }
+    setMidX(mid_x : number) {
+        this.mid_x = mid_x
+        this.x = this.mid_x - this.w / 2
     }
 }
 
-export class SvgTree extends Map<string, SvgNode> {
+export class Line {
+    parent_id: string
+    child_id: string
+    x1 : number
+    x2 : number
+    y1 : number
+    y2 : number
+    constructor(parent_id : string, child_id: string, x1 : number, y1 : number, x2 : number, y2 : number) {
+        this.parent_id = parent_id
+        this.child_id = child_id
+        this.x1 = x1
+        this.y1 = y1
+        this.x2 = x2
+        this.y2 = y2
+    }
+}
+
+export class SvgMap extends Map<string, SvgNode>{
+    width : number
+    height : number
+    lines : Array<Line> = new Array<Line>()
+    root_id : string
+    static line_offset_y = 3
+    static width_padding = 5
+    width_array : Array<number>
+    x_array : Array<number>
+
     constructor(tree_library : TreeLibrary, root_id : string) {
         super()
-        let nodes = [root_id].concat(tree_library.getDescendants(root_id))
-        for(let node of nodes) {
-
+        this.root_id = root_id
+        let ids = [root_id].concat(tree_library.getDescendants(root_id))
+        for(let id of ids) {
+            let tree = tree_library.get(id)
+            let node = new SvgNode(id, tree.type, tree.children_ids)
+            let [row, col] = tree_library.position_map.get(id)
+            node.row = row
+            node.col = col
+            this.set(id, node)
+            if(tree.children_ids.length == 0) { //this is a leaf
+                //add a word node
+                let word = new SvgNode('w'+id, tree.form, [])
+                word.row = node.row + 1
+                word.col = node.col
+                node.children = [word.id]
+                this.set(word.id, word)
+            }
         }
+        this.setFirstCoordinates(root_id)
+        this.generateLines()
+    }
+    setFirstCoordinates(id : string, depth : number = 0) {
+        let svg_node = this.get(id)
+        svg_node.row = depth;
+        svg_node.x = svg_node.col * SvgNode.dx
+        svg_node.y = (svg_node.row + 1) * SvgNode.dy
+        svg_node.calcTopBottom()
+        for(let child_id of svg_node.children)
+            this.setFirstCoordinates(child_id, depth+1)
+    }
+    generateLines() {
+        //generate lines
+        console.log(this)
+        this.lines = new Array<Line>()
+        for(let parent of Array.from(this.values())) {
+            for(let child_id of parent.children) {
+                let child = this.get(child_id)
+                let line = new Line(parent.id, child_id,
+                    parent.mid_x, parent.y + SvgMap.line_offset_y,
+                    child.mid_x, child.top_y - SvgMap.line_offset_y)
+                this.lines.push(line)
+            }
+        }
+    }
+    updateCoordinates() {
+        //first calculate max width of each column
+        let width_map = new Map<number, number>()
+        let max_col = 0
+        for(let node of this.values()) {
+            let current_w = width_map.get(node.col)
+            if(current_w == undefined || current_w < node.w)
+                width_map.set(node.col, node.w)
+            if(node.col > max_col) max_col = node.col
+        }
+        max_col += 1
+        this.width_array = new Array<number>()
+        this.x_array = new Array<number>()
+        for(let col = 0; col < max_col; col++) {
+            this.width_array.push(width_map.get(col) == undefined ?
+                0 : width_map.get(col) + SvgMap.width_padding)
+            this.x_array.push(col == 0 ? 0 : this.width_array[col-1] + this.x_array[col-1])
+        }
+        console.log(this.x_array)
+        console.log(this.width_array)
+        this.setMidpoints()
+        this.generateLines()
+    }
+    setMidpoints(id : string = this.root_id) {
+        let node = this.get(id)
+        node.calcTopBottom()
+        if(node.children.length == 0) {
+            node.setMidX(this.x_array[node.col] + this.width_array[node.col] / 2)
+            return
+        }
+        let midpoint_sum = 0
+        for(let child_id of node.children) {
+            this.setMidpoints(child_id)
+            midpoint_sum += this.get(child_id).mid_x
+        }
+        node.setMidX(midpoint_sum / node.children.length)
     }
 }
 
